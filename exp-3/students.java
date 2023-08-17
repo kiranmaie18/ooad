84da4b7788b474a03b5459710f2d30883717e5b8
kiranmaie18 <123735782+kiranmaie18@users.noreply.github.com>
2023-08-17 09:39:21 +0530

    Add files via upload

---
+++ exp-3/students.java
@@ -0,0 +1,130 @@
+
+
+/**
+ * Class students
+ */
+public class students {
+
+  //
+  // Fields
+  //
+
+  private void name;
+  private void address;
+  private void DOB;
+  private void subject;
+  private void college;
+  
+  //
+  // Constructors
+  //
+  public students () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of name
+   * @param newVar the new value of name
+   */
+  private void setName (void newVar) {
+    name = newVar;
+  }
+
+  /**
+   * Get the value of name
+   * @return the value of name
+   */
+  private void getName () {
+    return name;
+  }
+
+  /**
+   * Set the value of address
+   * @param newVar the new value of address
+   */
+  private void setAddress (void newVar) {
+    address = newVar;
+  }
+
+  /**
+   * Get the value of address
+   * @return the value of address
+   */
+  private void getAddress () {
+    return address;
+  }
+
+  /**
+   * Set the value of DOB
+   * @param newVar the new value of DOB
+   */
+  private void setDOB (void newVar) {
+    DOB = newVar;
+  }
+
+  /**
+   * Get the value of DOB
+   * @return the value of DOB
+   */
+  private void getDOB () {
+    return DOB;
+  }
+
+  /**
+   * Set the value of subject
+   * @param newVar the new value of subject
+   */
+  private void setSubject (void newVar) {
+    subject = newVar;
+  }
+
+  /**
+   * Get the value of subject
+   * @return the value of subject
+   */
+  private void getSubject () {
+    return subject;
+  }
+
+  /**
+   * Set the value of college
+   * @param newVar the new value of college
+   */
+  private void setCollege (void newVar) {
+    college = newVar;
+  }
+
+  /**
+   * Get the value of college
+   * @return the value of college
+   */
+  private void getCollege () {
+    return college;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void form_fileng()
+  {
+  }
+
+
+  /**
+   */
+  public void registretion()
+  {
+  }
+
+
+}
