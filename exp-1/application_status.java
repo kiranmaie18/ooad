a943790bd79f078a44c27685e2cb73eaa44606fb
kiranmaie18 <123735782+kiranmaie18@users.noreply.github.com>
2023-08-17 09:37:59 +0530

    Add files via upload

---
+++ exp-1/application_status.java
@@ -0,0 +1,55 @@
+
+
+/**
+ * Class application_status
+ */
+public class application_status {
+
+  //
+  // Fields
+  //
+
+  private void dispatched_application;
+  
+  //
+  // Constructors
+  //
+  public application_status () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of dispatched_application
+   * @param newVar the new value of dispatched_application
+   */
+  private void setDispatched_application (void newVar) {
+    dispatched_application = newVar;
+  }
+
+  /**
+   * Get the value of dispatched_application
+   * @return the value of dispatched_application
+   */
+  private void getDispatched_application () {
+    return dispatched_application;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void new_operation()
+  {
+  }
+
+
+}
