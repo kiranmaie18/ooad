0f2e3f71fe473bac9c91c3e92438e864c1a29cc6
kiranmaie18 <123735782+kiranmaie18@users.noreply.github.com>
2023-08-17 10:50:17 +0530

    Add files via upload

---
+++ exp-5/stock_details.java
@@ -0,0 +1,89 @@
+
+
+/**
+ * Class stock_details
+ */
+public class stock_details {
+
+  //
+  // Fields
+  //
+
+  private void take_order;
+  private void enter_order;
+  private void verify_the_order;
+  
+  //
+  // Constructors
+  //
+  public stock_details () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of take_order
+   * @param newVar the new value of take_order
+   */
+  private void setTake_order (void newVar) {
+    take_order = newVar;
+  }
+
+  /**
+   * Get the value of take_order
+   * @return the value of take_order
+   */
+  private void getTake_order () {
+    return take_order;
+  }
+
+  /**
+   * Set the value of enter_order
+   * @param newVar the new value of enter_order
+   */
+  private void setEnter_order (void newVar) {
+    enter_order = newVar;
+  }
+
+  /**
+   * Get the value of enter_order
+   * @return the value of enter_order
+   */
+  private void getEnter_order () {
+    return enter_order;
+  }
+
+  /**
+   * Set the value of verify_the_order
+   * @param newVar the new value of verify_the_order
+   */
+  private void setVerify_the_order (void newVar) {
+    verify_the_order = newVar;
+  }
+
+  /**
+   * Get the value of verify_the_order
+   * @return the value of verify_the_order
+   */
+  private void getVerify_the_order () {
+    return verify_the_order;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void delivery_the_item()
+  {
+  }
+
+
+}
