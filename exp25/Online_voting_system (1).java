5bad433280f591ae27651acec14f17b984a60829
kiranmaie18 <123735782+kiranmaie18@users.noreply.github.com>
2023-08-22 12:55:46 +0530

    Add files via upload

---
+++ exp25/Online_voting_system (1).java
@@ -0,0 +1,96 @@
+
+
+/**
+ * Class Online_voting_system
+ */
+public class Online_voting_system {
+
+  //
+  // Fields
+  //
+
+  private void voter_id;
+  private void login;
+  private void password;
+  
+  //
+  // Constructors
+  //
+  public Online_voting_system () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of voter_id
+   * @param newVar the new value of voter_id
+   */
+  private void setVoter_id (void newVar) {
+    voter_id = newVar;
+  }
+
+  /**
+   * Get the value of voter_id
+   * @return the value of voter_id
+   */
+  private void getVoter_id () {
+    return voter_id;
+  }
+
+  /**
+   * Set the value of login
+   * @param newVar the new value of login
+   */
+  private void setLogin (void newVar) {
+    login = newVar;
+  }
+
+  /**
+   * Get the value of login
+   * @return the value of login
+   */
+  private void getLogin () {
+    return login;
+  }
+
+  /**
+   * Set the value of password
+   * @param newVar the new value of password
+   */
+  private void setPassword (void newVar) {
+    password = newVar;
+  }
+
+  /**
+   * Get the value of password
+   * @return the value of password
+   */
+  private void getPassword () {
+    return password;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void get_voterid()
+  {
+  }
+
+
+  /**
+   */
+  public void get_password()
+  {
+  }
+
+
+}
---
+++ exp25/candidate.java
@@ -0,0 +1,106 @@
+
+
+/**
+ * Class candidate
+ */
+public class candidate {
+
+  //
+  // Fields
+  //
+
+  private void can_id;
+  private void symbol;
+  private void party_name;
+  private void facilities;
+  
+  //
+  // Constructors
+  //
+  public candidate () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of can_id
+   * @param newVar the new value of can_id
+   */
+  private void setCan_id (void newVar) {
+    can_id = newVar;
+  }
+
+  /**
+   * Get the value of can_id
+   * @return the value of can_id
+   */
+  private void getCan_id () {
+    return can_id;
+  }
+
+  /**
+   * Set the value of symbol
+   * @param newVar the new value of symbol
+   */
+  private void setSymbol (void newVar) {
+    symbol = newVar;
+  }
+
+  /**
+   * Get the value of symbol
+   * @return the value of symbol
+   */
+  private void getSymbol () {
+    return symbol;
+  }
+
+  /**
+   * Set the value of party_name
+   * @param newVar the new value of party_name
+   */
+  private void setParty_name (void newVar) {
+    party_name = newVar;
+  }
+
+  /**
+   * Get the value of party_name
+   * @return the value of party_name
+   */
+  private void getParty_name () {
+    return party_name;
+  }
+
+  /**
+   * Set the value of facilities
+   * @param newVar the new value of facilities
+   */
+  private void setFacilities (void newVar) {
+    facilities = newVar;
+  }
+
+  /**
+   * Get the value of facilities
+   * @return the value of facilities
+   */
+  private void getFacilities () {
+    return facilities;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void symbol_representation()
+  {
+  }
+
+
+}
