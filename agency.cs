b2333bccb7c27c7847faedb178dfc5d3c59479e6
kiranmaie18 <123735782+kiranmaie18@users.noreply.github.com>
2023-08-22 09:11:14 +0530

    Add files via upload

---
+++ agency.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// 
+/// </summary>
+public class agency
+{
+
+  #region Aggregations
+
+
+  #endregion
+
+  #region Compositions
+
+
+  #endregion
+
+  #region Attributes
+
+  /// <summary>
+  /// 
+  /// </summary>
+  private  description;
+
+
+
+  #endregion
+
+
+  #region Public methods
+
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <returns></returns>
+  public void send()
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <returns></returns>
+  public void request()
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  #endregion
+
+
+  #region Protected methods
+
+  #endregion
+
+
+  #region Private methods
+
+  #endregion
+
+
+}
+
