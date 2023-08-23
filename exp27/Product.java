ca58c83d5701b676140f557b1cb7c84f576769c8
kiranmaie18 <123735782+kiranmaie18@users.noreply.github.com>
2023-08-23 08:35:52 +0530

    Add files via upload

---
+++ exp27/Product.java
@@ -0,0 +1,96 @@
+
+
+/**
+ * Class Product
+ */
+public class Product {
+
+  //
+  // Fields
+  //
+
+  private void buy_product;
+  private void product_name;
+  private void product_number;
+  
+  //
+  // Constructors
+  //
+  public Product () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of buy_product
+   * @param newVar the new value of buy_product
+   */
+  private void setBuy_product (void newVar) {
+    buy_product = newVar;
+  }
+
+  /**
+   * Get the value of buy_product
+   * @return the value of buy_product
+   */
+  private void getBuy_product () {
+    return buy_product;
+  }
+
+  /**
+   * Set the value of product_name
+   * @param newVar the new value of product_name
+   */
+  private void setProduct_name (void newVar) {
+    product_name = newVar;
+  }
+
+  /**
+   * Get the value of product_name
+   * @return the value of product_name
+   */
+  private void getProduct_name () {
+    return product_name;
+  }
+
+  /**
+   * Set the value of product_number
+   * @param newVar the new value of product_number
+   */
+  private void setProduct_number (void newVar) {
+    product_number = newVar;
+  }
+
+  /**
+   * Get the value of product_number
+   * @return the value of product_number
+   */
+  private void getProduct_number () {
+    return product_number;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void amount()
+  {
+  }
+
+
+  /**
+   */
+  public void expiry_date()
+  {
+  }
+
+
+}
